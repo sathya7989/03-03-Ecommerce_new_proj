aa28c9b5a19ae16b142cd5a3d9a8b8813aaa0120
kalyanramnadhagiri <kr2235520@gmail.com>
2026-03-03 09:41:25 +0530

    Changes in main file

--- Ecommerce_Application/src/main/java/com/project/main/EcommerceApplication.java
+++ Ecommerce_Application/src/main/java/com/project/main/EcommerceApplication.java
@@ -8,7 +8,6 @@ public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
-		System.out.println("java is awesome");
 	}
 
 }
